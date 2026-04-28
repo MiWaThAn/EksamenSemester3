17e15f8dbd77ac006195c5f10cb9ff2f26d23e87
MartinDP333 <Martindauer448@gmail.com>
2026-04-28 13:46:29 +0200

    added infrastructure to create migrations to the database added DbContext made API have connection to DbContext added mcr.microsoft.com/mssql/server:2022-latest connection to docker compose

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
 
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
