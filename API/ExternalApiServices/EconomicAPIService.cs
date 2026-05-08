e811b806b5dda30aaf2ed0569d32499884cceb44
MartinDP333 <Martindauer448@gmail.com>
2026-05-08 14:08:49 +0200

    uncommented out mikkels code :)

--- API/ExternalApiServices/EconomicAPIService.cs
+++ API/ExternalApiServices/EconomicAPIService.cs
@@ -1,36 +1,36 @@
 ﻿
-//using Domain.Interfaces;
+using Domain.Interfaces;
 
 
-//namespace API.ExternalApiServices
-//{
-//    public class EconomicAPIService : IExternalAPIService
-//    {
-//        private readonly IHttpClientFactory _httpClientFactory;
-//        public EconomicAPIService(IHttpClientFactory httpClientFactory, ) 
-//        { 
-//            _httpClientFactory = httpClientFactory;
-//        }
+namespace API.ExternalApiServices
+{
+    public class EconomicAPIService : IExternalAPIService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public EconomicAPIService(IHttpClientFactory httpClientFactory, ) 
+        { 
+            _httpClientFactory = httpClientFactory;
+        }
 
-//        public async Task<IEnumerable<CustomerDTO>> GetCustomersAsync(//modtag setting)
-//        {
-//            var client = _httpClientFactory.CreateClient();
-//            var response = await client.GetAsync();
+        public async Task<IEnumerable<CustomerDTO>> GetCustomersAsync(//modtag setting)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync();
 
             
 
 
-//        }
+        }
 
-//        public Task<IEnumerable<ProjectDTO>> GetProjectsAsync(//modtag setting)
-//        {
-//            var client = _httpClientFactory.CreateClient();
-//            var response = await client.GetAsync()
-//        }
-//        public Task<IEnumerable<EmployeeDTO>> GetEmployeesAsync(//modtag setting)
-//        {
-//            var client = _httpClientFactory.CreateClient();
-//            var response = await client.GetAsync()
-//        }
-//    }
-//}
+        public Task<IEnumerable<ProjectDTO>> GetProjectsAsync(//modtag setting)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync()
+        }
+        public Task<IEnumerable<EmployeeDTO>> GetEmployeesAsync(//modtag setting)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync()
+        }
+    }
+}
